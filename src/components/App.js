635e1d51126ae0d50a4fc41af99c250945187b19
Rajbabbarsinh Rathva <83120772+Rajbabbarsinh@users.noreply.github.com>
2024-05-19 21:14:33 +0000

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="root">
+    <div id="main">
       <h1>Welcome to My React App</h1>
-      <p>I am learning React. My life is getting better</p>
+      <p id="root">I am learning React. My life is getting better</p>
     </div>
   )
 }
