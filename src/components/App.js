c69b05bd2ac36b5b984926d8f5805af4be184151
Rajbabbarsinh Rathva <83120772+Rajbabbarsinh@users.noreply.github.com>
2024-05-19 21:18:53 +0000

    .

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div id="main">
       <h1>Welcome to My React App</h1>
-      <p id="root">I am learning React. My life is getting better</p>
+      <p>I am learning React. My life is getting better</p>
     </div>
   )
 }
